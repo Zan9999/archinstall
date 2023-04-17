61f2096a38d27be60676d781ce7d0287257d3e98
Boria138 <boriabloger@protonmail.com>
2023-04-17 20:45:04 +0600

    Add driver tweaks

--- scripts/amd_driver.sh
+++ scripts/amd_driver.sh
@@ -2,3 +2,4 @@
 
 arch-chroot /mnt /bin/bash -c "pacman -S --needed --noconfirm mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader mesa-vdpau lib32-mesa-vdpau lib32-libva-mesa-driver"
 arch-chroot /mnt /bin/bash -c "sed -i s/'MODULES=()'/'MODULES=(amdgpu radeon)'/g /etc/mkinitcpio.conf"
+cp -rf ../tweaks/amd/* /mnt/etc/modprobe.d/
--- scripts/nvidia-open_driver.sh
+++ scripts/nvidia-open_driver.sh
@@ -2,3 +2,7 @@
 
 arch-chroot /mnt /bin/bash -c "pacman -S --needed --noconfirm nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-opencl-nvidia opencl-nvidia libxnvctrl"
 arch-chroot /mnt /bin/bash -c "sed -i s/'MODULES=()'/'MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)'/g /etc/mkinitcpio.conf"
+cp -rf ../tweaks/nvidia/nvidia.conf /mnt/etc/modprobe.d/nvidia-tweaks.conf
+cp -rf ../tweaks/nvidia/nvidia-uvm.conf /mnt/etc/modules-load.d/
+cp -rf ../tweaks/nvidia/60-nvidia.rules /mnt/etc/udev/rules.d/
+cp -rf ../tweaks/nvidia/nvidia-tweaks.hook /mnt//usr/share/libalpm/hooks
