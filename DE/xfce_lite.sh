7459aa8c62bca6e3dceff5670b1bc8fe3f5c0113
Boria138S <boriabloger@protonmail.com>
2023-05-01 21:11:00 +0600

    Delete XFCE lite

--- DE/xfce_lite.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-arch-chroot /mnt /bin/bash -c "pacman -S --needed gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb xfce4 lightdm lightdm-gtk-greeter"
-arch-chroot /mnt /bin/bash -c "systemctl enable lightdm"
