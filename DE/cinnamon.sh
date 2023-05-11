1a962c5aa54de75849c3b7e0c4f0c58367fe79b6
Boria138 <boriabloger@protonmail.com>
2023-05-11 16:20:21 +0600

    Enable realtime scheduling support for gnome-shell on Wayland

--- DE/cinnamon.sh
+++ DE/cinnamon.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 arch-chroot /mnt /bin/bash -c "pacman -S --needed gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb cinnamon cinnamon-control-center nemo-fileroller nemo-preview cinnamon-translations gucharmap xed lightdm lightdm-gtk-greeter parole xfce4-terminal xfce4-taskmanager network-manager-applet gthumb metacity blueberry gnome-shell"
-arch-chroot /mnt /bin/bash -c "gsettings set org.cinnamon.muffin check-alive-timeout 15000"
 arch-chroot /mnt /bin/bash -c "systemctl enable lightdm"
