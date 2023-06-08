9d0790932cddcd87cbbf98474f71a8df4de13fd2
AndiZan999 <47110479+Zan9999@users.noreply.github.com>
2023-06-08 16:17:00 +0000

    Update kde_lite.sh

--- DE/kde_lite.sh
+++ DE/kde_lite.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-arch-chroot /mnt /bin/bash -c "pacman -S --needed konsole plasma-wayland-session plasma power-profiles-daemon kwalletmanager kio-admin xsettingsd"
+arch-chroot /mnt /bin/bash -c "pacman -S --needed konsole xorg xorg-server plasma power-profiles-daemon kwalletmanager kio-admin xsettingsd"
 arch-chroot /mnt /bin/bash -c "systemctl enable sddm"
